547447030dd8497031fa04aaf4c227651c895fc4
Yenthe <53619412+crm-go@users.noreply.github.com>
2024-06-26 17:09:23 +0200

    undo changes and validate for Xrm.page.data.entity

--- src/eventHandler.js
+++ src/eventHandler.js
@@ -24,7 +24,7 @@
             const newLocation = window.location.origin + apiUrl + pluralName + '(' + recordId + ')';
             return newLocation;
         } catch (e) {
-            showNotification(e.message, `error`);
+            alert('Error occurred: ' + e.message);
         }
     }
 
@@ -51,98 +51,11 @@
         }
     }
 
-    function showNotification(message, type = 'info', time = 3500) {
-
-        //this has been bothering me for a long time.
-        if (message == "Cannot read properties of null (reading 'entity')") {
-            message = "Please open a form to use PrettifyMyWebApi";
-        }
-
-        const SlideTime = 250;
-
-        // Call the function to inject the element if not present yet!
-        injectNotificationElement();
-
-        // Construct class name
-        var className = `pmwa-notification-${type}`;
-
-        // Get the notification element
-        var notification = document.getElementById("pmwa-notification");
-
-        // Update the notification content
-        notification.querySelector('span').innerHTML = message;
-
-        // Update the notification class
-        notification.className = ''; // Clear existing classes
-        notification.classList.add(className); // Add new class
-
-        // Show the notification
-        notification.style.display = 'block';
-        notification.style.opacity = 1;
-        notification.style.transition = `opacity ${SlideTime}ms`;
-
-
-        // Set a timer to hide the notification
-        setTimeout(function () {
-            notification.style.opacity = 0;
-            notification.style.transition = `opacity ${SlideTime}ms`;
-            setTimeout(function () {
-                notification.style.display = 'none';
-            }, SlideTime);
-        }, time);
-    }
-
-    function injectNotificationElement() {
-
-        // pmwa-notification already injected
-        if (document.getElementById('pmwa-notification')) { return; }
-
-        // Create a new style element
-        var styleElement = document.createElement('style');
-        styleElement.textContent = `
-        #pmwa-notification {
-            font-family: SegoeUI, "Segoe UI";
-            position: absolute;
-            min-width: 200px;
-            right: 30px;
-            top: 80px;
-            display: none;
-            padding: 20px;
-            font-size: 15px;
-		    border-radius:5px;
-        }
-
-        .pmwa-notification-info {
-            background-color: #ccf6ff;
-            color: #056;
-            border: 1px solid #9ef;
-        }
-
-        .pmwa-notification-error {
-    	    background-color: #ffd6e0;
-            color: #661429;
-            border:1px solid #ffadc2;
-        }
-        `;
-
-        // Append the style element to the head
-        document.head.appendChild(styleElement);
-
-        // Create a new div element
-        var notificationDiv = document.createElement('div');
-        notificationDiv.id = 'pmwa-notification';
-
-        // Create a new span element
-        var spanElement = document.createElement('span');
-
-        // Append the span to the div
-        notificationDiv.appendChild(spanElement);
-
-        // Append the notification div to the body
-        document.body.appendChild(notificationDiv);
-    }
-
     if (window.Xrm && window.Xrm.Page) {
+        if (!window.Xrm.Page.data || !window.Xrm.Page.data.entity || !window.Xrm.Utility) {
+            alert(`Please open a form to use PrettifyMyWebApi`);
+            return;
+        }
         const newLocation = await getWebApiUrl() + '#p';
         window.postMessage({ action: 'openInWebApi', url: newLocation });
     } else if (/\/api\/data\/v[0-9][0-9]?.[0-9]\//.test(window.location.pathname)) {
